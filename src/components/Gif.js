4148e2e99c23fd21cf8718f6f5ce1c1ece9a5241
Cyrielle Marchand <cyrielle.marchand@gmail.com>
2023-10-18 17:14:38 +0200

    added condition if chosen gif is null

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -4,6 +4,9 @@ function Gif({gifID, updateGifID}) {
     updateGifID(gifID)
   }
 
+  if (!gifID) {
+    return <p>Loading...</p>
+  }
   return (
     <img src={`https://media3.giphy.com/media/${gifID}/100.gif?cid=ecf05e47wijw9r5qoasj4ckqtv4r6losgah3mt3smjxd8v3t&ep=v1_gifs_trending&rid=100.gif&ct=g`} alt="" className="gif" onClick={handleClick} />
   )
